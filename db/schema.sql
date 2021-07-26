df21c468fcfa2164b96e5300b4f5e97f8ad46688
rangamboa <rangamboa@gmail.com>
2021-07-26 16:23:20 -0700

    database work

--- db/schema.sql
+++ db/schema.sql
@@ -11,8 +11,8 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INT,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
 
   FOREIGN KEY (department_id)
     REFERENCES department(id)
@@ -23,7 +23,7 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT,
+  role_id INT NOT NULL,
   manager_id INT,
 
   FOREIGN KEY (role_id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,11 +2,19 @@ USE staff_db;
 
 INSERT INTO department (name)
 VALUES  ('Management'),
-        ('Marketing'),
-        ('Sales');
+        ('Computed Tomography'),
+        ('MRI');
 
 INSERT INTO role (title, salary, department_id)
-VALUES  ('Manager', 85000, 1),
-        ('Marketing Coordinator', 60000, 2),
-        ('Account Representative', 50000, 3),
-        ('District Lead', 75000, 3);
+VALUES  ('Manager', 125000, 1),
+        ('Assistant Manager', 100000, 1),
+        ('CT Tech', 60000, 2),
+        ('MRI Tech', 70000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Penny', 'Craig', 1, NULL),
+        ('Ran', 'Gamboa', 2, 1),
+        ('Jason', 'Garbes', 3, 2),
+        ('Ken', 'Puffenbarger', 3, 2),
+        ('Daniela', 'Vela', 4, 2),
+        ('Corrie', 'Ferguson', 4, 2);
