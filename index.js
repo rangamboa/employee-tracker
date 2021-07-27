b3a674204675c707e98c41a1acb9af12716a42b9
rangamboa <rangamboa@gmail.com>
2021-07-26 22:33:43 -0700

    initial coding for connecting inquirer with database

--- index.js
+++ index.js
@@ -4,40 +4,58 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const path = require('path');
+let choice;
 
 // Set up express.
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.static('public'));
-
-// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
-
-// app.get('/send', (req, res) =>cd ..
-//   res.sendFile(path.join(__dirname, 'public/sendFile.html'))
-// );
-
-// app.get('/routes', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/routes.html'))
-// );
+const app = express();
 
 // app.listen(PORT, () =>
-//   console.log(`Example app listening at http://localhost:${PORT}`)
+//     console.log(`\n----- Listening at http://localhost:${PORT}`)
 // );
 
-// console.table([
-//     {
-//       name: 'foo',
-//       age: 10
-//     }, {
-//       name: 'bar',
-//       age: 20
-//     }
-//   ]);
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 
+// Parse request body as JSON.
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+  
+        // MySQL username,
+        user: 'root',
+  
+        // MySQL password
+
+        password: 'password',
+        database: 'staff_db'
+    },
+
+    console.log(`\n----- Connected to the movies_db database. -----`)
+);
+  
+app.get('/', (req, res) => res.send('Main'));
+
+// app.get('/', (req, res) => {
+
+//     // res.json(`${req.method} request received in /api/movies.`);
+//     console.log('----- GET request in /.');
+  
+//     res.send('movie data');
+  
+//     // db.query('SELECT * FROM employee;', function (err, results) {
+//     //   console.log(results);
+//     //   res.json(results);
+// });
+  
 function init() {
 
-    console.log('\n\n----- Welcome to Employee Tracker! -----\n');
+    console.log('\n----- Welcome to Employee Tracker! -----');
 
     // Call main menu function.
     mainMenu();
@@ -52,17 +70,21 @@ const mainMenu = () => {
         {
             type: 'list',
             name: 'task',
-            prefix: '!',
-            message: 'Please select an option:',
+            prefix: '-',
+            message: 'Please select an option from the following menu:',
             choices: ['1. View All Departments', '2. View All Roles', '3. View All Employees', '4. Add a Department', '5. Add a Role' ,'6. Add an Employee','7. Update an Employee Role'],
         },
         ])
         .then((answers) => {
 
-            console.log(answers.task[0]);
+            choice = answers.task[0];
+            console.log(choice);
+
+            if (answers.task[0])
 
         });
     return;
 }
 
+// Initialize on application launch.
 init();
