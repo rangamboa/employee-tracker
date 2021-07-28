1ec0623046fb03c40f50da397ffec4f82d3f7ab6
rangamboa <rangamboa@gmail.com>
2021-07-27 17:34:53 -0700

    query code

--- db/query.sql
+++ db/query.sql
@@ -7,6 +7,14 @@ SELECT *
 SELECT *
     FROM employee; 
 
-SELECT role.title, role.id, department.name, role.salary
+SELECT id AS ID, name AS Department FROM department;
+
+SELECT role.title AS Title, role.id AS ID, department.name AS Department, role.salary AS Salary
     FROM role
-    JOIN department ON role.department_id = department.id;
+    JOIN department
+    ON role.department_id = department.id;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title
+    FROM employee
+    JOIN role
+    ON employee.role_id = role.id;
